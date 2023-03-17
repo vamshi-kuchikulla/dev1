3877752a034acf38022eb230486f7ec08fbe1281
vamshi-kuchikulla <vamshi.kuchikulla@gmail.com>
2023-03-16 23:08:24 -0400

    CST-1 Task to add Date & UUID function

---
+++ dev1Date.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class dev1Date{
+  public static void main(String[] args) {
+    System.out.println("in dev"+ LocalDate.now());
+  }
+}
---
+++ dev1UUID.java
@@ -0,0 +1,7 @@
+import java.util.UUID;
+
+public class dev1UUID{
+    public static void main(String[] args) {
+      System.out.println("in dev"+ UUID.randomUUID());
+    }
+  }
